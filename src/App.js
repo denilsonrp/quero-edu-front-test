d662c0078982dbe99960ea6e456db4338c3c3a57
Denilson <denilsonraimundo17@gmail.com>
2020-10-20 19:48:25 -0300

    :tada: Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Home from './pages/Home'
+
+import './styles/base/_base.scss'
+import './styles/base/_normalize.scss'
+
+const App = () => {
+
+  return <Home />
+
+}
+
+export default App
