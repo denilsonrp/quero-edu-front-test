4755725532be71995a1c51118af402c45c8a488b
Denilson <denilsonraimundo17@gmail.com>
2020-10-28 12:16:42 -0300

    :lipstick: Create responsively version of menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,18 +2,31 @@ import React from 'react'
 
 import './styles.scss'
 
+import { FaAngleDown } from "react-icons/fa"
+
 const Menu = () => {
 
   return (
     <div className="menu">
-      <ul className="container">
-        <li>
-          <a href="/">Pré-matrículas</a>
-        </li>
-        <li className="-active">
-          <a href="/">Bolsas favoritas</a>
-        </li>
-      </ul>
+      <div className="container">
+        <h3 className="section">Minha conta</h3>
+
+        <label className="label-menu" htmlFor="chkMenu">
+          Menu
+          <FaAngleDown/>
+        </label>
+
+        <input type="checkbox" id="chkMenu" className="_hide" />
+
+        <ul>
+          <li>
+            <a href="/">Pré-matrículas</a>
+          </li>
+          <li className="-active">
+            <a href="/">Bolsas favoritas</a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 
